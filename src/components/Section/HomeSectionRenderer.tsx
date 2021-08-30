c9c21b94cb7c801d7a32b86cb93d44182e8f9504
brouk2426 <brouk2426@gmail.com>
2021-08-30 17:15:13 +0900

    feat: Change rendering order

--- src/components/Section/HomeSectionRenderer.tsx
+++ src/components/Section/HomeSectionRenderer.tsx
@@ -31,7 +31,7 @@ function HomeSectionRenderer(props: HomeSectionRendererProps) {
     order,
     totalSectionLength,
   } = props;
-
+  console.log(section.slug, order);
   if (!section.items) {
     // 사파리 disk cache 된 섹션 정보가 호출 될 때
     // 특정섹션에 items 필드가 자체가 존재하지 않는 경우를 방어합니다.
@@ -48,8 +48,8 @@ function HomeSectionRenderer(props: HomeSectionRendererProps) {
   const renderFreeWebtoon = () => {
     if (
       genre === 'webtoon'
-      && ((totalSectionLength >= 5 && order === 4)
-        || (totalSectionLength < 5 && section.slug === 'home-webtoon-daily-bestseller'))
+      && ((totalSectionLength >= 4 && order === 3)
+        || (totalSectionLength < 4 && section.slug === 'home-webtoon-reading-books'))
     ) {
       return <FreeWebtoonSection genre={genre} />;
     }
